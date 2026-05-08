3b9add2203d919edfa2bc153ee8b4a8a5239e205
Esther-FaBer <165953395+Esther-FaBer@users.noreply.github.com>
2026-05-08 23:21:38 +0100

    properties

--- models/properties.js
+++ models/properties.js
@@ -3,22 +3,40 @@ const db = require("../db/connection");
 exports.fetchProperties = async () =>{
 
     const { rows: properties } = await db.query(
-        `SELECT property_id, name AS property_name, location, price_per_night, CONCAT(first_name, ' ', surname) AS host
-        FROM properties JOIN users ON host_id = user_id;
-        `);
+        `SELECT 
+            p.property_id,
+            p.name AS property_name,
+            p.location,
+            p.price_per_night,
+            CONCAT(u.first_name, ' ', u.surname) AS host,
+            ROUND(AVG(r.rating), 1) AS avg_rating
+        FROM properties p
+        JOIN users u ON p.host_id = u.user_id
+        LEFT JOIN reviews r ON p.property_id = r.property_id
+        GROUP BY p.property_id, u.user_id`
+    );
     
-    return properties;
+    return rows;
 };
 
 
 exports.fetchPropertyById =  async (property_id) => {
 
     const { rows: [property] } = await db.query(
-        `SELECT property_id, name, location, price_per_night, description, 
-        CONCAT(first_name, ' ', surname) AS host_name, avatar
-        FROM properties
-        JOIN users ON properties.host_id = users.user_id
-        WHERE property_id= $1`,
+        `SELECT 
+            p.property_id,
+            p.name,
+            p.location,
+            p.price_per_night,
+            p.description,
+            CONCAT(u.first_name, ' ', u.surname) AS host_name,
+            u.avatar,
+            ROUND(AVG(r.rating), 1) AS avg_rating
+        FROM properties p
+        JOIN users u ON p.host_id = u.user_id
+        LEFT JOIN reviews r ON p.property_id = r.property_id
+        WHERE p.property_id = $1
+        GROUP BY p.property_id, u.user_id`,
         [property_id]
     );
     return property;
