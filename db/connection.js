6af53e71a4971821e63638aeca494f140ab07ce6
Esther-FaBer <165953395+Esther-FaBer@users.noreply.github.com>
2026-05-19 22:12:36 +0100

    debugging

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,6 @@
 const { Pool } = require("pg");
-
 const ENV = process.env.NODE_ENV || "development";
-
 require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
-
-
-console.log(`ENV: ${ENV}`);
 
 
 const config = {};
@@ -13,18 +8,19 @@ const config = {};
 if(ENV === "production") {
     config.connectionString = process.env.DATABASE_URL;
     config.max = 2;
-    console.log(`Connecting to production DB...`);
-    console.log(`DATABASE_URL set: ${!!process.env.DATABASE_URL}`);
+
 }
 
 const pool = new Pool(config);
 
-pool.on('connect', () => {
-    console.log('Connected to database');
-});
-
-pool.on('error', (err) => {
-    console.error('Database pool error:', err);
-});
+if (ENV !== "test") {
+    console.log(`ENV: ${ENV}`);
+    pool.on('connect', () => {
+        console.log('Connected to database');
+    });
+    pool.on('error', (err) => {
+        console.error('Database pool error:', err);
+    });
+}
 
 module.exports = pool;
